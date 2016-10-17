9933b8c4b92cdf6abec626aff31fc64354293638
josnewton <jnewton@jlab.org>
2016-10-17 10:53:27 -0400

    Update PIDExamination.java

--- PIDExamination.java
+++ PIDExamination.java
@@ -1,4 +1,4 @@
-package org.jlab.service.eb;
+package org.jlab.clas.ebuilder;
 
 import static java.lang.Math.abs;
 import java.util.HashMap;
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 /**
  *
- * @author jnewton
+ * @author Joseph Newton
  */
 public class PIDExamination {
     
@@ -160,7 +160,120 @@ public class PIDExamination {
             return truth;
     }
 
-  
+     public HashMap<Integer,PIDExamination> getElectronTests(DetectorParticle particle) {
+                
+                HashMap<Integer,PIDExamination>  electrontests = new HashMap<Integer, PIDExamination>();
+               
+                PIDExamination test1 = new PIDExamination();
+                test1.setClosestBeta(true);
+                test1.setHTCC(true);
+                test1.setCorrectSF(true);
+         
+                
+                PIDExamination test2 = new PIDExamination();
+                test2.setClosestBeta(false);
+                test2.setHTCC(true);
+                test2.setCorrectSF(true);
+                
+                PIDExamination test3 = new PIDExamination();
+                test3.setClosestBeta(true);
+                test3.setHTCC(false);
+                test3.setLTCC(false);
+                
+                PIDExamination test4 = new PIDExamination();
+                test4.setClosestBeta(true);
+                test4.setHTCC(true);
+                test4.setCorrectSF(false);
+                test4.setHTCCThreshold(false);
+                
+                PIDExamination test5 = new PIDExamination();
+                test5.setClosestBeta(false);
+                test5.setHTCC(true);
+                test5.setCorrectSF(false);
+                test5.setHTCCThreshold(false);
+                
+                electrontests.put(0,test1);
+                electrontests.put(1,test2);
+                electrontests.put(2,test3);
+                electrontests.put(3,test4);
+                electrontests.put(4,test5);
+   
+                return electrontests;
+            }
+    
+      public HashMap<Integer,PIDExamination> getPionTests(DetectorParticle particle) {
+                
+                HashMap<Integer,PIDExamination>  piontests = new HashMap<Integer, PIDExamination>();
+               
+                PIDExamination test1 = new PIDExamination();
+                test1.setClosestBeta(true);
+                test1.setHTCC(false);
+                test1.setCorrectSF(false);
+              
+                
+                PIDExamination test2 = new PIDExamination();
+                test2.setClosestBeta(true);
+                test2.setHTCC(true);
+                test2.setCorrectSF(false);
+                test2.setHTCCThreshold(true);
+                
+                PIDExamination test3 = new PIDExamination();
+                test3.setClosestBeta(false);
+                test3.setHTCC(true);
+                test3.setCorrectSF(false);
+                test3.setHTCCThreshold(true);
+                
+                PIDExamination test4 = new PIDExamination();
+                test4.setClosestBeta(true);
+                test4.setHTCC(false);
+                test4.setLTCC(true);
+                test4.setLTCCThreshold(true);
+                
+                PIDExamination test5 = new PIDExamination();
+                test5.setClosestBeta(false);
+                test5.setHTCC(true);
+                test5.setLTCC(false);
+                test5.setLTCCThreshold(false);
+                
+                piontests.put(0,test1);
+                piontests.put(1,test2);
+                piontests.put(2,test3);
+                piontests.put(3,test4);
+                piontests.put(4,test5);
+
+                return piontests;
+            }                
+
+                
+      public HashMap<Integer,PIDExamination> getProtonTests(DetectorParticle particle) {
+                
+                HashMap<Integer,PIDExamination>  protontests = new HashMap<Integer, PIDExamination>();
+               
+                PIDExamination test1 = new PIDExamination();
+           
+                test1.setClosestBeta(true);
+                test1.setHTCC(false);
+                //test1.setLTCC(false);
+
+                protontests.put(0, test1);
+
+                return protontests;
+            }
+    
+        public HashMap<Integer,PIDExamination> getKaonTests(DetectorParticle particle) {
+                
+                HashMap<Integer,PIDExamination>  kaontests = new HashMap<Integer, PIDExamination>();
+               
+                PIDExamination test1 = new PIDExamination();
+           
+                test1.setClosestBeta(true);
+                test1.setHTCC(false);
+                //test1.setLTCC(false);
+
+                kaontests.put(0, test1);
+
+                return kaontests;
+            }     
     
     public Integer getCandidateID(){return this.CandidateID;}
     public Boolean getClosestBeta(){return this.closestBeta;}
