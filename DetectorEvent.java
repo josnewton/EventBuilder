c1513a0ce4dbc396fac149d9cebc0d13eda09b7f
josnewton <jnewton@jlab.org>
2016-10-17 10:56:27 -0400

    Update DetectorEvent.java

--- DetectorEvent.java
+++ DetectorEvent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.jlab.clas.detector;
+package org.jlab.clas.ebuilder;
 
 
 
@@ -141,32 +141,7 @@ public class DetectorEvent {
         return map;
     }        
     
-       public double CalculateStartTime(int usertriggerid){
-            double t_0r = 0;
-            DetectorParticle particle = new DetectorParticle();
-            particle = this.trigger.getTriggerParticle();
-            if(particle.hasHit(DetectorType.FTOF, 2)==true){
-            double beta = 0.0;
-            if(abs(usertriggerid)==11){
-                beta = 1;//We assign electron beta if at least one track has responses in FTOF/HTCC
-            }
-            if(abs(usertriggerid)==211){
-                beta = particle.getTheoryBeta(211); //We Assign Pion mass to the fastest negative track
-            }
-            if(abs(usertriggerid)==22){
-                beta = particle.getTheoryBeta(22);
-            }
 
-            double t_0 = 0,deltatr = 0, t_rf = 0, z_0 = 0, m = 1000000;
-            t_0r = particle.getTime(DetectorType.FTOF) - (particle.getPathLength(DetectorType.FTOF))/(beta*29.9792);
-            deltatr = t_0r - t_rf - (particle.vertex().z() - z_0)/beta + m*2.0004;
-            t_0 = deltatr%2.0004 - 2.0004/2;
-
-            }
-   
-      
-            return t_0r;
-        }
     
     public boolean hasElectron(){
         boolean x = false;
