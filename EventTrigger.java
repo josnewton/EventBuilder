b792d8db171a42e6d62da1d0a58221709414bd34
josnewton <jnewton@jlab.org>
2016-10-19 10:31:53 -0400

    Update EventTrigger.java

--- EventTrigger.java
+++ EventTrigger.java
@@ -1,5 +1,6 @@
 package org.jlab.clas.ebuilder;
 
+import static java.lang.Math.abs;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,20 +10,22 @@ import org.jlab.geom.prim.Line3D;
 import org.jlab.geom.prim.Path3D;
 import org.jlab.geom.prim.Point3D;
 import org.jlab.clas.detector.DetectorEvent;
+import org.jlab.detector.base.DetectorType;
 
 
 /**
  *
- * @author Joseph Newton
+ * @author gavalian
  */
 public class EventTrigger {
     
  private double zt=0.0;
  private double rftime=0.0;
  private double starttime=0.0;
- private List<Integer> UserTriggerIDs = new ArrayList<Integer>();//List of Trigger Particles Inputted By The User
  private DetectorParticle triggerparticle = new DetectorParticle();
-    
+ private HashMap<Integer,DetectorParticle> ElectronCandidates = new HashMap<Integer,DetectorParticle>();
+ private HashMap<Integer,DetectorParticle> PositronCandidates = new HashMap<Integer,DetectorParticle>();
+
 
     
     public EventTrigger(){
@@ -33,7 +36,9 @@ public class EventTrigger {
     public void   setRFTime(float rf){this.rftime = rf;}
     public void   setStartTime(double start){this.starttime = start;}
     public void   setTriggerParticle(DetectorParticle particle){this.triggerparticle=particle;}
-    public void   setUserTriggerIDs(List<Integer> user){this.UserTriggerIDs = user;}
+    public void   setElectronCandidates(HashMap<Integer,DetectorParticle> ecandidates){this.ElectronCandidates = ecandidates;}
+    public void   setPositronCandidates(HashMap<Integer,DetectorParticle> epluscandidates){this.PositronCandidates = epluscandidates;}
+
 
 
     public DetectorParticle GetBestTriggerParticle(HashMap<Integer,DetectorParticle> TriggerCandidates) {
@@ -42,7 +47,7 @@ public class EventTrigger {
             HashMap<Integer,Integer> Scores = new HashMap<Integer,Integer>();
 
             for(int i = 0 ; i < SizeOfMap; i++){
-              Scores.put(i,TriggerCandidates.get(i).getTIDResult().getScore()); 
+              Scores.put(i,TriggerCandidates.get(i).getScore()); 
             }
             int HighestScore = GetHighestScore(Scores);
             int SizeofMap = TriggerCandidates.size();
@@ -77,7 +82,7 @@ public class EventTrigger {
             return FastestParticle;
         }
             
-    public int GetHighestScore(HashMap<Integer,Integer> Sc){
+    public int GetHighestScore(HashMap<Integer,Integer> Sc) {
            int max = Sc.get(0);
 
             for (int i = 1; i < Sc.size(); i++) {
@@ -89,7 +94,7 @@ public class EventTrigger {
             return max;
         }
         
-    public int GetHighestMomentumIndex(HashMap<Integer,Double> momentum){
+    public int GetHighestMomentumIndex(HashMap<Integer,Double> momentum) {
             Double max = momentum.get(0);
             int MaximumIndex = 0;
             for (int i = 1; i < momentum.size(); i++) {
@@ -102,11 +107,38 @@ public class EventTrigger {
             return MaximumIndex;
         }
     
+       public double StartTime(DetectorParticle particle, int usertriggerid){
+            double t_0r = 0;
+
+            if(particle.hasHit(DetectorType.FTOF, 2)==true){
+            double beta = 0.0;
+            if(abs(usertriggerid)==11){
+                beta = 1;//We assign electron beta if at least one track has responses in FTOF/HTCC
+            }
+            if(abs(usertriggerid)==211){
+                beta = particle.getTheoryBeta(211); //We Assign Pion mass to the fastest negative track
+            }
+            if(abs(usertriggerid)==22){
+                beta = particle.getTheoryBeta(22);
+            }
+
+            double t_0 = 0,deltatr = 0, t_rf = 0, z_0 = 0, m = 1000000;
+            t_0r = particle.getTime(DetectorType.FTOF) - (particle.getPathLength(DetectorType.FTOF))/(beta*29.9792);
+            deltatr = t_0r - t_rf - (particle.vertex().z() - z_0)/beta + m*2.0004;
+            t_0 = deltatr%2.0004 - 2.0004/2;
+
+            }
+   
+      
+            return t_0r;
+        }
+    
     public double getZt(){ return this.zt;}
     public double getRFTime(){ return this.rftime; }
     public double getStartTime(){ return this.starttime;}
     public DetectorParticle getTriggerParticle(){return this.triggerparticle;}
-    public List<Integer> getUserTriggerIDs(){return this.UserTriggerIDs;}
+    public HashMap<Integer,DetectorParticle> getElectronCandidates(){return this.ElectronCandidates;}
+    public HashMap<Integer,DetectorParticle> getPositronCandidates(){return this.PositronCandidates;}
 
     
 
